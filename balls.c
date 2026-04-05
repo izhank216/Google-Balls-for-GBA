1894515967a4ef2a2d1aad680a5ac844367da0dc
izhank216 <izhank216@gmail.com>
2026-04-05 19:19:32 +0500

    Update balls.c

--- balls.c
+++ balls.c
@@ -1,4 +1,3 @@
-#include <gba.h>
 #include <tonc.h>
 #include <stdlib.h>
 
@@ -21,6 +20,7 @@ void init_balls() {
 
     for(int i = 0; i < BALL_COUNT; i++) {
         int idx = i % 6;
+        // Spread balls out slightly around the logo coordinates
         balls[i].ox = balls[i].x = TO_FP(x_coords[idx] + (i % 30));
         balls[i].oy = balls[i].y = TO_FP(y_coords[idx] + (i / 10));
         balls[i].vx = balls[i].vy = 0;
@@ -35,27 +35,37 @@ void update_balls(int mx, int my) {
     for(int i = 0; i < BALL_COUNT; i++) {
         int dx = f_mx - balls[i].x;
         int dy = f_my - balls[i].y;
-        int dist_sq = (FROM_FP(dx)*FROM_FP(dx)) + (FROM_FP(dy)*FROM_FP(dy));
+        
+        // Use fixed point for distance check
+        int d_x = FROM_FP(dx);
+        int d_y = FROM_FP(dy);
+        int dist_sq = (d_x * d_x) + (d_y * d_y);
 
+        // Interaction: Push away
         if(dist_sq < 1600) {
-            balls[i].vx -= dx / 4;
-            balls[i].vy -= dy / 4;
+            balls[i].vx -= dx / 8;
+            balls[i].vy -= dy / 8;
         }
 
+        // Return to home
         balls[i].vx += (balls[i].ox - balls[i].x) / 16;
         balls[i].vy += (balls[i].oy - balls[i].y) / 16;
+        
+        // Friction
         balls[i].vx = (balls[i].vx * 7) / 8;
         balls[i].vy = (balls[i].vy * 7) / 8;
+        
         balls[i].x += balls[i].vx;
         balls[i].y += balls[i].vy;
     }
 }
 
 int main() {
+    // Mode 3 (Bitmap), Background 2 enabled
     REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
     
     irq_init(NULL);
-    irq_enable(II_VBLANK);
+    irq_add(II_VBLANK, NULL);
 
     init_balls();
 
@@ -64,26 +74,37 @@ int main() {
 
     while(1) {
         VBlankIntrWait();
-        
-        u16 mouse_data = REG_SIODATA32;
-        int dx = (int8_t)(mouse_data & 0xFF);
-        int dy = (int8_t)((mouse_data >> 8) & 0xFF);
-        
-        mx += dx;
-        my += dy;
+        key_poll();
 
-        if(mx < 0) mx = 0; if(mx > 239) mx = 239;
-        if(my < 0) my = 0; if(my > 159) my = 159;
+        // Mouse/Cursor control via D-Pad
+        mx += 2 * key_tri_horz();
+        my += 2 * key_tri_vert();
 
+        // Keep cursor on screen
+        mx = clamp(mx, 0, 239);
+        my = clamp(my, 0, 159);
+
+        // Clear screen with White
         m3_fill(CLR_WHITE);
+
         for(int i = 0; i < BALL_COUNT; i++) {
-            m3_plot(FROM_FP(balls[i].x), FROM_FP(balls[i].y), balls[i].color);
-            m3_plot(FROM_FP(balls[i].x)+1, FROM_FP(balls[i].y), balls[i].color);
-            m3_plot(FROM_FP(balls[i].x), FROM_FP(balls[i].y)+1, balls[i].color);
+            int draw_x = FROM_FP(balls[i].x);
+            int draw_y = FROM_FP(balls[i].y);
+            
+            // Draw 2x2 ball for visibility
+            m3_plot(draw_x, draw_y, balls[i].color);
+            m3_plot(draw_x + 1, draw_y, balls[i].color);
+            m3_plot(draw_x, draw_y + 1, balls[i].color);
+            m3_plot(draw_x + 1, draw_y + 1, balls[i].color);
         }
         
         update_balls(mx, my);
+        
+        // Draw cursor
         m3_plot(mx, my, CLR_BLACK);
+        m3_plot(mx+1, my, CLR_BLACK);
+        m3_plot(mx, my+1, CLR_BLACK);
     }
+    
     return 0;
 }
