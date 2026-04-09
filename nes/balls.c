bbb82b9c9553aca2fbbe89843d1bdddd7ed5ba31
izhank216 <izhank216@gmail.com>
2026-04-09 15:20:23 +0500

    Create balls.c

---
+++ nes/balls.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include <string.h>
+#include "neslib.h"
+
+#define BALL_COUNT 64
+#define FP_SHIFT 4
+#define TO_FP(n) ((n) << FP_SHIFT)
+#define FROM_FP(n) ((n) >> FP_SHIFT)
+
+unsigned char x[BALL_COUNT];
+unsigned char y[BALL_COUNT];
+unsigned char ox[BALL_COUNT];
+unsigned char oy[BALL_COUNT];
+signed char vx[BALL_COUNT];
+signed char vy[BALL_COUNT];
+unsigned char colors[BALL_COUNT];
+
+const unsigned char x_raw[] = {202, 148, 156, 114, 165, 100, 194, 45, 168, 73, 194, 135, 153, 136, 108, 121, 8, 110, 146, 145, 146, 169, 173, 148, 194, 167, 78, 194, 117, 137, 14, 131, 25, 133, 73, 127, 119, 156, 144, 94, 97, 105, 42, 10, 166, 166, 178, 100, 143, 59, 27, 132, 123, 6, 63, 6, 108, 169, 137, 118, 126, 101, 126, 17};
+const unsigned char y_raw[] = {78, 83, 69, 59, 36, 78, 59, 88, 52, 83, 6, 62, 42, 52, 41, 70, 60, 81, 65, 49, 34, 69, 99, 120, 41, 114, 67, 23, 83, 80, 71, 80, 82, 46, 13, 35, 46, 81, 88, 32, 56, 75, 4, 27, 55, 88, 34, 65, 32, 5, 9, 116, 32, 38, 62, 49, 36, 43, 37, 58, 100, 46, 108, 17};
+
+const unsigned char palette[] = {
+    0x0f, 0x00, 0x10, 0x30,
+    0x0f, 0x06, 0x16, 0x26,
+    0x0f, 0x01, 0x11, 0x21,
+    0x0f, 0x09, 0x19, 0x29,
+    0x0f, 0x08, 0x18, 0x28
+};
+
+void init_balls() {
+    unsigned char i;
+    for(i = 0; i < BALL_COUNT; i++) {
+        ox[i] = x[i] = x_raw[i] + 30;
+        oy[i] = y[i] = y_raw[i] + 40;
+        vx[i] = 0;
+        vy[i] = 0;
+
+        if (x_raw[i] < 60)       colors[i] = 1;
+        else if (x_raw[i] < 100) colors[i] = 0;
+        else if (x_raw[i] < 140) colors[i] = 3;
+        else if (x_raw[i] < 180) colors[i] = 1;
+        else if (x_raw[i] < 210) colors[i] = 2;
+        else                     colors[i] = 0;
+    }
+}
+
+void update_balls(unsigned char mx, unsigned char my) {
+    unsigned char i;
+    signed int dx, dy;
+    
+    for(i = 0; i < BALL_COUNT; i++) {
+        dx = (signed int)mx - x[i];
+        dy = (signed int)my - y[i];
+
+        if(abs(dx) + abs(dy) < 25) {
+            vx[i] -= (dx >> 2);
+            vy[i] -= (dy >> 2);
+        }
+
+        vx[i] += ((signed char)ox[i] - (signed char)x[i]) >> 3;
+        vy[i] += ((signed char)oy[i] - (signed char)y[i]) >> 3;
+
+        vx[i] = (vx[i] * 4) / 5;
+        vy[i] = (vy[i] * 4) / 5;
+
+        x[i] += (vx[i] >> 2);
+        y[i] += (vy[i] >> 2);
+    }
+}
+
+void main(void) {
+    unsigned char mx = 128;
+    unsigned char my = 120;
+    unsigned char pad;
+    unsigned char i;
+    unsigned char oam_id;
+
+    pal_bg(palette);
+    pal_spr(palette);
+    bank_spr(1);
+    init_balls();
+    
+    ppu_on_all();
+
+    while(1) {
+        ppu_wait_nmi();
+        oam_id = 0;
+
+        pad = pad_poll(0);
+        if(pad & PAD_LEFT)  mx -= 2;
+        if(pad & PAD_RIGHT) mx += 2;
+        if(pad & PAD_UP)    my -= 2;
+        if(pad & PAD_DOWN)  my += 2;
+
+        oam_id = oam_spr(mx, my, 0x03, 0, oam_id);
+
+        for(i = 0; i < BALL_COUNT; i++) {
+            oam_id = oam_spr(x[i], y[i], 0x00, colors[i], oam_id);
+        }
+
+        update_balls(mx, my);
+    }
+}
